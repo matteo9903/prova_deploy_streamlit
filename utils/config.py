dcac8e4c989f49cbd8765c0cf316a117dc4cee25
matpice <m.piceni@reply.it>
2024-11-18 10:27:23 +0100

    update secrets import

--- utils/config.py
+++ utils/config.py
@@ -6,9 +6,9 @@ load_dotenv('.env')
 import streamlit as st
 
 # Google Oauth 2.0 env
-CLIENT_ID = st.secrets("CLIENT_ID")
-CLIENT_SECRET = st.secrets("CLIENT_SECRET")
-REDIRECT_URI = st.secrets("REDIRECT_URI")
+CLIENT_ID = st.secrets["CLIENT_ID"]
+CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
+REDIRECT_URI = st.secrets["REDIRECT_URI"]
 
 # prova
-SECRET_PROVA = st.secrets("SECRET_PROVA")
+SECRET_PROVA = st.secrets["SECRET_PROVA"]
